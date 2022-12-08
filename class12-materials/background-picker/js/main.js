e8d0746995707288f72d86aca3bce3329779c0d3
SouravsGithub <souravsemail2001@gmail.com>
2022-12-08 21:51:28 +0530

    refactored the js

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -2,6 +2,7 @@ const naruto = document.querySelector(".naruto");
 const sasuke = document.querySelector(".sasuke");
 const itachi = document.querySelector(".itachi");
 const nameOfCharacter = document.querySelector("h1");
+const body = document.querySelector("body");
 
 naruto.addEventListener("click", showNaruto);
 sasuke.addEventListener("click", showSasuke);
@@ -10,25 +11,24 @@ itachi.addEventListener("click", showItachi);
 function showNaruto(event) {
   document.querySelector("body").style.background =
     "url(https://www.nawpic.com/media/2020/naruto-4k-nawpic-11.jpg)";
-  document.querySelector("body").style.backgroundSize = "cover";
-  document.querySelector("body").style.backgroundRepeat = "no-repeat";
-  document.querySelector("body").style.backgroundPosition = "center";
+  body.style.backgroundSize = "cover";
+  body.style.backgroundRepeat = "no-repeat";
+  body.style.backgroundPosition = "center";
   nameOfCharacter.innerText = "NARUTO";
-  document.querySelector("body").style.animation = "appear 5s linear infiite";
 }
 function showSasuke(event) {
   document.querySelector("body").style.background =
     "url(https://free4kwallpapers.com/uploads/originals/2015/08/30/sasuke-uchiha-shippuden.jpg)";
-  document.querySelector("body").style.backgroundSize = "cover";
-  document.querySelector("body").style.backgroundRepeat = "no-repeat";
-  document.querySelector("body").style.backgroundPosition = "bottom";
+  body.style.backgroundSize = "cover";
+  body.style.backgroundRepeat = "no-repeat";
+  body.style.backgroundPosition = "bottom";
   nameOfCharacter.innerText = "SASUKE";
 }
 function showItachi(event) {
   document.querySelector("body").style.background =
     "url(https://wallpaperaccess.com/full/4834078.jpg)";
-  document.querySelector("body").style.backgroundSize = "cover";
-  document.querySelector("body").style.backgroundRepeat = "no-repeat";
-  document.querySelector("body").style.backgroundPosition = "center";
+  body.style.backgroundSize = "cover";
+  body.style.backgroundRepeat = "no-repeat";
+  body.style.backgroundPosition = "center";
   nameOfCharacter.innerText = "ITACHI";
 }
